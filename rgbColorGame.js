9872f70a8b8190400ce5b382a4155ece9bce367e
cK <wim.va@mail.com>
2018-02-26 09:13:58 +0100

    Overzicht veld

--- rgbColorGame.js
+++ rgbColorGame.js
@@ -1,1 +1,23 @@
-console.log("Welcome to the game");
+var vierkanten = document.querySelectorAll(".vierkant");
+var aantalVierkanten = 6;
+var kleuren = [];
+
+function speelSpel() {
+    inkleuring(aantalVierkanten)
+    for (var i = 0; i < aantalVierkanten; i++) {
+        vierkanten[i].style.backgroundColor = kleuren[i];
+    }
+}
+
+function inkleuring(x) {
+    for (var i = 0; i < x; i++) {
+        var kleur = "";
+        var rood = Math.floor(Math.random() * 256);
+        var groen = Math.floor(Math.random() * 256);
+        var blauw = Math.floor(Math.random() * 256);
+        kleur = "rgb(" + rood + ", " + groen + ", " + blauw + ")";
+        kleuren.push(kleur);
+    }
+}
+
+speelSpel();
